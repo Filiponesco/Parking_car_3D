85b2e303b0840bae54dbb4c49ed048658a39ad29
Filiponesco <fifigaw98@gmail.com>
2019-11-20 01:38:41 +0100

    create project

---
+++ Prototype 1/Assets/Scripts/CarEngine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarEngine : MonoBehaviour
+{
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Drive();
+    }
+    private void Drive()
+    {
+        wheelFL.motorTorque = 10000f;
+        wheelFR.motorTorque = 10000f;
+    }
+}
