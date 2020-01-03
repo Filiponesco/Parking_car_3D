ed91841e9413cbc790014313936cc5c6c6bb4c4d
Matikones <matiadamek97@gmail.com>
2020-01-04 00:56:36 +0100

    Menu and Pause

--- Prototype 1/Assets/MainMenu.cs
+++ Prototype 1/Assets/MainMenu.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Text city;
     public void PlayGame()
     {
         SceneManager.LoadScene("City1");
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(currentLevel, LoadSceneMode.Additive);
     }
 
+    public void FreeRoam1()
+    {
+        SceneManager.LoadScene("City1");
+    }
+    public void FreeRoam2()
+    {
+        SceneManager.LoadScene("City2");
+    }
     public void QuitGame()
     {
         Debug.Log("QUIT");
