95b67e6f9595fc565498e2c6f69f3280acd43d13
Filiponesco <fifigaw98@gmail.com>
2020-01-07 23:39:47 +0100

    add physics to cybertruck, add to prefabs
    To do: lights, special effects

--- Prototype 1/Assets/Cars/Scripts/TruckUserControl.cs
+++ Prototype 1/Assets/Cars/Scripts/TruckUserControl.cs
@@ -4,7 +4,7 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
-    [RequireComponent(typeof (CarController))]
+    [RequireComponent(typeof (TruckController))]
     public class TruckUserControl : MonoBehaviour
     {
         public TruckController m_Car; // the car controller we want to use
